4cf55bcd05283ee89ed4ada8e6ef31c83355781c
Samiksha Bhore <samikshabhore7636@gmail.com>
2026-07-12 09:56:34 +0530

    Ratate array to left by 1

---
+++ Traversal/rotatearraybyOne.java
@@ -0,0 +1,37 @@
+package Traversal;
+
+import java.util.*;
+
+public class rotatearraybyOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of length of array: ");
+        int len = sc.nextInt();
+
+        int arr[] = new int[len];
+
+        System.out.print("Enter Array: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+
+        int temp = arr[0];
+        for (int i = 0; i < arr.length-1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = temp;
+
+        System.out.println("Array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+
+    }
+}
